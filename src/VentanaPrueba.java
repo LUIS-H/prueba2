7ecd7b09c624c3e00f6f8d10a4dce780d04c64d8
Luis <Luis@DESKTOP-UB04DUN>
2020-10-08 08:18:15 -0500

    1.1

---
+++ src/VentanaPrueba.java
@@ -0,0 +1,14 @@
+/**
+ * Hola
+ * @author Luis
+ */
+import javax.swing.*;
+
+public class VentanaPrueba {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Mi ventana");        
+        jf.setSize(500, 600);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+}
